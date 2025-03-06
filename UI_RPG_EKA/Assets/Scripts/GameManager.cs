6da4ec61b99d7ebc82e451bf83271c8684dd0cef
Darkuu <rihardszorgis@gmail.com>
2025-03-06 15:19:50 +0200

    Added Shouting

---
+++ UI_RPG_EKA/Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Player player;
+    public Enemy enemy;
+    public Character character;
+
+    void Start()
+    {
+        enemy.Shout();
+        player.Shout();
+        character.Shout();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
